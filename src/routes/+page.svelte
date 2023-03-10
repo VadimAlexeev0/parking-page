1a569bdc51b7f62f28a8b982769fcfcbc97d63f3
Vadim Alexeev <valexeev30@gmail.com>
2023-03-10 17:51:47 +0000

    Styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="grid h-screen place-items-center">
-    <div class="bg-slate-800 rounded-xl p-8 text-zinc-200 w-3/5 h-2/5 font-sans colours flex flex-col drop-shadow-2xl justify-around">
-        <h1 class="text-8xl"><b>Arkiv</b> Coming Soon...</h1>
+    <div class="bg-slate-800 rounded-xl p-16 text-zinc-200 w-3/5 h-auto font-sans colours flex flex-col drop-shadow-2xl justify-around">
+        <h1 class="text-6xl"><b>Arkiv</b> Coming Soon...</h1>
         <h2 class="text-3xl">Your personal archive of live streams</h2>
         <p class="">Vadim Alexeev 2023</p>
     </div>
