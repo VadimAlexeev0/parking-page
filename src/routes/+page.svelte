572ab318d26931caa229bbdffff017c12a133c99
Vadim Alexeev <valexeev30@gmail.com>
2023-03-10 17:56:10 +0000

    punctuation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="grid h-screen place-items-center">
     <div class="bg-slate-800 rounded-xl p-16 text-zinc-200 w-3/5 h-auto font-sans colours flex flex-col drop-shadow-2xl justify-around">
         <h1 class="text-6xl"><b>Arkiv</b> Coming Soon...</h1>
-        <h2 class="text-3xl">Your personal archive of live streams</h2>
+        <h2 class="text-3xl">Your personal archive of live streams.</h2>
         <p class="">Vadim Alexeev 2023</p>
     </div>
 </div>
